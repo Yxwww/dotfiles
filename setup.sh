72b6ae0ad65375646f23252e26e953a33de53c8b
Yxwww <yuxi.wangcd@gmail.com>
2019-03-29 02:18:01 -0600

    Use spaceship-prompt

--- setup.sh
+++ setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-echo "Brew setup with yarn"
-brew install yarn
 
-echo "Setup nvim"
-brew install neovim
-yarn global add neovim
+# setup spaceship-prompt https://github.com/denysdovhan/spaceship-prompt
+# Faster startup than oh-my-zsh: 70ms vs 400ms
+yarn global add spaceship-prompt
 
-echo "link dotfiles"
-./godotfiles -force
+# echo "Brew setup with yarn"
+# brew install yarn
+#
+# echo "Setup nvim"
+# brew install neovim
+# yarn global add neovim
+#
+# echo "link dotfiles"
+# ./linkdotfiles.sh
+#
