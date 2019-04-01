fc4044cface3a5732792fcfe4c436c21662dcf11
Yxwww <yuxi.wangcd@gmail.com>
2019-04-01 00:58:36 -0600

    Improve setup scripts, add tmux iterm2 cursor line config

--- dotfiles/.vimrc
+++ dotfiles/.vimrc
@@ -67,6 +67,7 @@ filetype plugin indent on
 " NOTE: cursor chagne on mode switching is a terminal feature. MacOS
 " terminal does not support cursor change
 
+let $NVIM_TUI_ENABLE_CURSOR_SHAPE = 1
 if exists('$TMUX')
   let &t_SI = "\<Esc>Ptmux;\<Esc>\<Esc>]50;CursorShape=1\x7\<Esc>\\"
   let &t_SR = "\<Esc>Ptmux;\<Esc>\<Esc>]50;CursorShape=2\x7\<Esc>\\"
--- setup.sh
+++ setup.sh
@@ -2,15 +2,31 @@
 
 # setup spaceship-prompt https://github.com/denysdovhan/spaceship-prompt
 # Faster startup than oh-my-zsh: 70ms vs 400ms
-yarn global add spaceship-prompt
+setup_zsh_spaceship() {
+  brew install zsh
+  yarn global add spaceship-prompt
+}
 
-# echo "Brew setup with yarn"
-# brew install yarn
-#
+setup_js() {
+  brew install yarn
+}
+
 # echo "Setup nvim"
-# brew install neovim
-# yarn global add neovim
-#
+setup_neovim() {
+  brew install neovim
+  yarn global add neovim
+}
+
 # echo "link dotfiles"
-# ./linkdotfiles.sh
-#
+link_dotfiles() {
+  ./linkdotfiles.sh
+}
+
+
+# Tmux setup
+## Clipper setup
+setup_clipper() {
+  brew install clipper
+  brew services start clipper
+}
+
