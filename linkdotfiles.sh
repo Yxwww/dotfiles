2c928b56be14fc0aa061c0fa0e5a9773e0fb31f7
Yxwww <yuxi.wangcd@gmail.com>
2019-05-11 17:14:42 -0600

    Add global gitignore

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -1,3 +1,7 @@
 # link dotfiles to ~/
 ln -sf "$(pwd)"/dotfiles/{*,.[^.],.??*} ~/
 
+# git ignore
+GIT_CONFIG_DIR=~/.config/git
+mkdir -p $GIT_CONFIG_DIR
+ln -sf "$(pwd)"/configs/git/ignore $GIT_CONFIG_DIR/
