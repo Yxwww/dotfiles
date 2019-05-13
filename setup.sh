756f1fc907441d558d63a14a647111478ecf66bb
Yxwww <yuxi.wangcd@gmail.com>
2019-05-13 01:34:52 -0600

    Add more docs for setting up llvm+clang with ccls

--- setup.sh
+++ setup.sh
@@ -37,8 +37,17 @@ setup_zsh_autosuggestions() {
 
 setup_cpp_dev() {
   ln -sf ./configs/coc-settings.json ~/.vim/
-  brew install ccls
   brew install ctags
   brew install ninja
-  # if missing system headers run: open /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14. pkg
+
+  ## Setup llvm+clang & ccls follows https://github.com/MaskRay/ccls/wiki/Build step by step
+
+  ## SYSTEM HEADER SETUP on OSX :
+  ## 1. if missing system headers run: open /Library/Developer/CommandLineTools/Packages/macOS_SDK_headers_for_macOS_10.14. pkg
+
+  ## 2. Use shell script wrapper to wrap ccls to explicitly use the correct CommandLineTools c++ lib instead of the default one
+  ## to provide proper c++ lib linting support: https://github.com/MaskRay/ccls/issues/191#issuecomment-453809905
+  #!/bin/sh
+  #exec /path/to/ccls/Release/ccls -init='{"clang":{"extraArgs":["-isystem", "/Library/Developer/CommandLineTools/usr/include/c++/v1"]}}' "$@"
+  ##
 }
