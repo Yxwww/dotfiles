a4f5e70577d110746646f0d91ab6e5991bd7d4ed
Yuxi Wang <yuxi.wangcd@gmail.com>
2019-08-23 19:39:24 -0600

    Move coc-settings to config/

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -7,7 +7,7 @@ mkdir -p $GIT_CONFIG_DIR
 ln -sf "$(pwd)"/configs/git/ignore $GIT_CONFIG_DIR/
 
 # coc-settings
-ln -sf "$(pwd)"/configs/coc-settings.json ~/.config/nvim/
+ln -sf "$(pwd)"/configs/vim/coc-settings.json ~/.config/nvim/
 
 # vim deps
 ln -snf "$(pwd)"/configs/vim/config ~/.vim/config
