e6a80c129bfb5e525a62b74e65e7e086dc870f84
Yuxi Wang <yuxi.wangcd@gmail.com>
2020-08-22 15:42:43 -0600

    Disable no explicit any

--- dotfiles/.eslintrc.js
+++ dotfiles/.eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
   },
   overrides: [{
     "files": ["*.js", "*.jsx"],
