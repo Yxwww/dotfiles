f2b0dee0b3ae58d6861fe874cea37623e71728ba
Yuxi Wang <yuxi.wangcd@gmail.com>
2021-12-25 16:34:25 -0700

    Add telescope keybindings

--- configs/vim/config/telescope.lua
+++ configs/vim/config/telescope.lua
@@ -1,7 +1,35 @@
+local actions = require "telescope.actions"
+
 require('telescope').setup{
   pickers = {
     find_files = {
       hidden = true
+    }
+  },
+  defaults = {
+   mappings = {
+      i = {
+        ["<C-n>"] = actions.cycle_history_next,
+        ["<C-p>"] = actions.cycle_history_prev,
+
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+
+        ["<C-c>"] = actions.close,
+
+        ["<CR>"] = actions.select_default,
+
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
+
+        ["<PageUp>"] = actions.results_scrolling_up,
+        ["<PageDown>"] = actions.results_scrolling_down,
+
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_better,
+
+        ["<C-q>"] = actions.send_to_qflist + actions.open_qflist,
+      }
     }
   }
 }
