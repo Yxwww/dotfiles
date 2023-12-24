289a69b148d921683689d24a2926bba25ff6fe66
Yuxi Wang <yuxi.wangcd@gmail.com>
2023-12-23 17:40:53 -0700

    Update lazy-lock
    - update link script

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -7,7 +7,7 @@ mkdir -p $GIT_CONFIG_DIR
 ln -sf "$(pwd)"/configs/git/ignore $GIT_CONFIG_DIR/
 
 # nvim
-ln -snf "$(pwd)"/configs/nvim/init.vim ~/.config/nvim/
+ln -snf "$(pwd)"/configs/nvim ~/.config/nvim
 
 # iterm2
 ln -snf "$(pwd)"/configs/iterm/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
@@ -19,7 +19,6 @@ ln -snf "$(pwd)"/configs/iterm/com.googlecode.iterm2.plist ~/Library/Preferences
 # node
 ln -sf "$(pwd)"/configs/node/package.json ~/
 # ln -sf "$(pwd)"/configs/node/yarn.lock ~/
-
 
 # alacritty
 ln -sf "$(pwd)"/configs/alacritty ~/.config
@@ -33,4 +32,3 @@ ln -sf "$(pwd)"/configs/starship.toml ~/.config
 # code
 ln -sf "$(pwd)"/configs/Code/keybindings.json ~/Library/Application\ Support/Code/User/
 ln -sf "$(pwd)"/configs/Code/settings.json ~/Library/Application\ Support/Code/User/
-
