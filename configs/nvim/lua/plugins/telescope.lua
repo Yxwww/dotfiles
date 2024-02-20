5591359a35635374f77ddfaa07240416d54f78cf
Yuxi Wang <yuxi.wangcd@gmail.com>
2024-02-19 22:28:49 -0800

    Update telescope to display filename first

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -6,11 +6,30 @@ local actions = require('telescope.actions')require('telescope').setup{
   }
 }
 
+local function filenameFirst(_, path)
+  local tail = vim.fs.basename(path)
+  local parent = vim.fs.dirname(path)
+  if parent == "." then return tail end
+  return string.format("%s\t\t%s", tail, parent)
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "TelescopeResults",
+  callback = function(ctx)
+    vim.api.nvim_buf_call(ctx.buf, function()
+      vim.fn.matchadd("TelescopeParent", "\t\t.*$")
+      vim.api.nvim_set_hl(0, "TelescopeParent", { link = "Comment" })
+    end)
+  end,
+})
+
+
 return {
   {
     "nvim-telescope/telescope.nvim",
     opts = {
       defaults = {
+        path_display = filenameFirst,
         mappings = {
           i = {
             ["<C-n>"] = actions.cycle_history_next,
