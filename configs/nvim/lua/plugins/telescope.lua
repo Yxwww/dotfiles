eb9130a63f4ef2a031234d5124ff88669cc71102
Yuxi Wang <yuxi.wangcd@gmail.com>
2024-02-25 17:10:09 -0800

    Add border

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -2,7 +2,11 @@ local actions = require('telescope.actions')require('telescope').setup{
   pickers = {
     buffers = {
       sort_lastused = true
-    }
+    },
+    diagnostics = {
+      theme = "ivy",
+      initial_mode = "normal",
+    },
   }
 }
 -- local path_display = require("telescope.builtin").find_files{ path_display = { "truncate" } }
@@ -28,6 +32,106 @@ local actions = require('telescope.actions')require('telescope').setup{
 return {
   {
     "nvim-telescope/telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      {
+        "nvim-telescope/telescope-file-browser.nvim",
+        config = function (_, opts) 
+          local fb_actions = require("telescope").extensions.file_browser.actions
+          opts.extensions =  {
+            file_browser = {
+              theme = "dropdown",
+              -- disables netrw and use telescope-file-browser in its place
+              hijack_netrw = true,
+              mappings = {
+                -- your custom insert mode mappings
+                ["n"] = {
+                  -- your custom normal mode mappings
+                  ["N"] = fb_actions.create,
+                  ["h"] = fb_actions.goto_parent_dir,
+                  ["/"] = function()
+                    vim.cmd("startinsert")
+                  end,
+                  ["<C-u>"] = function(prompt_bufnr)
+                    for i = 1, 10 do
+                      actions.move_selection_previous(prompt_bufnr)
+                    end
+                  end,
+                  ["<C-d>"] = function(prompt_bufnr)
+                    for i = 1, 10 do
+                      actions.move_selection_next(prompt_bufnr)
+                    end
+                  end,
+                  ["<PageUp>"] = actions.preview_scrolling_up,
+                  ["<PageDown>"] = actions.preview_scrolling_down,
+                },
+              },
+            },
+          }
+          require("telescope").load_extension("file_browser")
+        end
+      }
+    },
+    keys = {
+      {
+        "<leader>fm",
+        function()
+          local telescope = require("telescope")
+
+          local function telescope_buffer_dir()
+            return vim.fn.expand("%:p:h")
+          end
+
+          telescope.extensions.file_browser.file_browser({
+            path = "%:p:h",
+            cwd = telescope_buffer_dir(),
+            respect_gitignore = false,
+            hidden = true,
+            grouped = true,
+            previewer = false,
+            initial_mode = "normal",
+            layout_config = { height = 40 },
+          })
+        end,
+        desc = "Open File Browser with the path of the current buffer",
+      }
+    },
+
+		--   dependencies = {
+		-- 	{
+		-- 		"nvim-telescope/telescope-fzf-native.nvim",
+		-- 		build = "make",
+		-- 	},
+		-- 	"nvim-telescope/telescope-file-browser.nvim",
+		-- },
+		--   keys = {
+		-- 		";f",
+		-- 		function()
+		-- 			local telescope = require("telescope")
+		--
+		-- 			local function telescope_buffer_dir()
+		-- 				return vim.fn.expand("%:p:h")
+		-- 			end
+		--
+		-- 			telescope.extensions.file_browser.file_browser({
+		-- 				path = "%:p:h",
+		-- 				cwd = telescope_buffer_dir(),
+		-- 				respect_gitignore = false,
+		-- 				hidden = true,
+		-- 				grouped = true,
+		-- 				previewer = false,
+		-- 				initial_mode = "normal",
+		-- 				layout_config = { height = 40 },
+		-- 			})
+		-- 		end,
+		-- 		desc = "Open File Browser with the path of the current buffer",
+		--   },
     opts = {
       defaults = {
         path_display = { 'shorten' },
@@ -57,21 +161,21 @@ return {
           }
         }
       },
+      
     },
   },
 
 
   -- add telescope-fzf-native
-  {
-    "telescope.nvim",
-    dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
-    },
-  },
+  -- {
+  --   "telescope.nvim",
+  --   dependencies = {
+  --     "nvim-telescope/telescope-fzf-native.nvim",
+  --     build = "make",
+  --     config = function()
+  --       require("telescope").load_extension("fzf")
+  --     end,
+  --   },
+  -- },
 
 }
-
