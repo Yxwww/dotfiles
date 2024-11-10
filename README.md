edd1478a206410e55b2e92c217d9677a984a55fe
Yuxi Wang <yuxi.wangcd@gmail.com>
2024-11-09 21:25:01 -0800

    Fix blink config

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 ## Setup Environment
+
 ```bash
 ./setup.sh
 ```
 
 ## link dotfiles
+
 ```bash
 ./linkdotfiles.sh
 ```
 
-
 ## setup
 
 ### Tmux italic
+
 - [link on medium](https://medium.com/@dubistkomisch/how-to-actually-get-italics-and-true-colour-to-work-in-iterm-tmux-vim-9ebe55ebc2be)
-Steps:
+  Steps:
 
 1. `tic -x ./scripts/xterm-256color-italic.terminfo`
 2. `tic -x ./scripts/tmux-256color.terminfo`
 3. set $TERM in .zshrc to `tmux-256color.terminfo`
-4. set tmux.conf 
+4. set tmux.conf
+
 ```
 set -g default-terminal 'tmux-256color'
 set -as terminal-overrides ',xterm*:Tc:sitm=\E[3m'
 ```
+
 5. configure vimrc
+
 ```
 let &t_8f="\<Esc>[38;2;%lu;%lu;%lum"
 let &t_8b="\<Esc>[48;2;%lu;%lu;%lum"
@@ -31,6 +36,11 @@ set termguicolors
 ```
 
 optional: for SSH
+
 ```
 alias ssh='TERM=xterm-256color ssh'
 ```
+
+### Tig binding:
+
+https://github.com/jonas/tig/wiki/Bindings
--- configs/nvim/lua/plugins/editor.lua
+++ configs/nvim/lua/plugins/editor.lua
@@ -19,13 +19,14 @@ return {
         severity_sort = true,
       },
     },
-    on_attach = function(client, bufnr)
-      vim.notify("attached!")
-      if vim.api.nvim_buf_line_count(bufnr) > 10000 then
-        vim.notify(vim.api.nvim_buf_line_count(bufnr))
-        client.stop()
-      end
-    end,
+    dependencies = { "saghen/blink.cmp" },
+    -- config = function(_, opts)
+    --   local lspconfig = require("lspconfig")
+    --   for server, config in pairs(opts.servers) do
+    --     config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+    --     lspconfig[server].setup(config)
+    --   end
+    -- end,
   },
   { "hrsh7th/nvim-cmp", enabled = false },
   {
@@ -86,46 +87,10 @@ return {
   {
     "yetone/avante.nvim",
     event = "VeryLazy",
-    lazy = false,
-    version = false, -- set this if you want to always pull the latest change
     opts = {
       -- add any opts here
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
+      behavior = {
+        -- auto_suggestions = true,
       },
     },
   },
