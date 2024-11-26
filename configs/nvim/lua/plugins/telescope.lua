1d5edc5dd743c89c466cb65ed629e50115bbadb6
Yuxi Wang <yuxi.wangcd@gmail.com>
2024-11-25 21:54:17 -0800

    fix tmux prefix-s command

--- configs/nvim/lua/plugins/telescope.lua
+++ configs/nvim/lua/plugins/telescope.lua
@@ -31,8 +31,8 @@ return {
         file_ignore_patterns = { "esm", "cjs", "dist" },
         mappings = {
           i = {
-            ["<C-Down>"] = actions.cycle_history_next,
-            ["<C-Up>"] = actions.cycle_history_prev,
+            ["<C-j>"] = actions.cycle_history_next,
+            ["<C-k>"] = actions.cycle_history_prev,
             -- ["<C-n>"] = actions.cycle_history_next,
             -- ["<C-p>"] = actions.cycle_history_prev,
 
