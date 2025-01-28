9de4cb06b5b444962351f7931396b465272f9bf3
Yuxi Wang <yuxi.wangcd@gmail.com>
2025-01-27 22:13:50 -0800

    feat(vim): use claude-3.5 as default model

---
+++ configs/nvim/lua/plugins/ai.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = "claude-3.5-sonnet",
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
