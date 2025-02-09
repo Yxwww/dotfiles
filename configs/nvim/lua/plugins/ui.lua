b8e2425ddcd852a40a8e8d89043bd62c9a0e8e14
Yuxi Wang <yuxi.wangcd@gmail.com>
2025-02-08 17:34:55 -0800

    update(nvim): disable c-h in oil

--- configs/nvim/lua/plugins/ui.lua
+++ configs/nvim/lua/plugins/ui.lua
@@ -12,6 +12,24 @@ return {
         -- Show files and directories that start with "."
         show_hidden = true,
       },
+      keymaps = {
+        -- ["g?"] = { "actions.show_help", mode = "n" },
+        -- ["<CR>"] = "actions.select",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
+        -- ["<C-t>"] = { "actions.select", opts = { tab = true } },
+        -- ["<C-p>"] = "actions.preview",
+        -- ["<C-c>"] = { "actions.close", mode = "n" },
+        -- ["<C-r>"] = "actions.refresh",
+        -- ["-"] = { "actions.parent", mode = "n" },
+        -- ["_"] = { "actions.open_cwd", mode = "n" },
+        -- ["`"] = { "actions.cd", mode = "n" },
+        -- ["~"] = { "actions.cd", opts = { scope = "tab" }, mode = "n" },
+        -- ["gs"] = { "actions.change_sort", mode = "n" },
+        -- ["gx"] = "actions.open_external",
+        -- ["g."] = { "actions.toggle_hidden", mode = "n" },
+        -- ["g\\"] = { "actions.toggle_trash", mode = "n" },
+      },
     },
     -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
@@ -141,14 +159,14 @@ return {
     ---@class snacks.Config
     opts = {
       picker = {
-        win ={
-          input={
-            keys={
-                ["<c-k>"] = { "history_back", mode = { "i", "n" } },
-                ["<c-j>"] = { "history_forward", mode = { "i", "n" } },
-            }
-          }
-        }
+        win = {
+          input = {
+            keys = {
+              ["<c-k>"] = { "history_back", mode = { "i", "n" } },
+              ["<c-j>"] = { "history_forward", mode = { "i", "n" } },
+            },
+          },
+        },
       },
       dashboard = {
         preset = {
